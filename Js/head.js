7d9ec368c067563714b87e34a6e1e080bc7c488e
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-24 07:17:14 +1000

    Update head.js

--- Js/head.js
+++ Js/head.js
@@ -1,9 +1,7 @@
     var Head = "
-        <head>
         <script src='/TubeYou/Js/Header.js'></script>
         <link href='https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css2?family=Baloo+Chettan+2&display=swap' rel='stylesheet' type='text/css'>
         <link rel='icon' type='image/png' sizes='64x64' href='https://thuwd.github.io/cdn/cdn/gba/img/404.png'>
         <link href='https://fonts.googleapis.com/css2?family=Sen&display=swap' rel='stylesheet'>
-        </head>
                "
