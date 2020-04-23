26d0b51faf379dcb081d418f411e7a1bdadedaf7
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-24 07:57:38 +1000

    Update welcome.js

--- Js/welcome.js
+++ Js/welcome.js
@@ -1,5 +1,7 @@
 document.body.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
         window.location.replace("/TubeYou/App");
+        alert('welcome')
+        alert('the games have only just begun')
     }
 });
