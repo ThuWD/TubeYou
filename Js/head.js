c4263ff0724970c2654c53f78500ccdf5ad2aa62
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-24 06:49:02 +1000

    Create head.js

---
+++ Js/head.js
@@ -0,0 +1,23 @@
+    var Head = "
+        <head>
+        <script src='/TubeYou/Js/Header.js'></script>
+        <link href='https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css2?family=Baloo+Chettan+2&display=swap' rel='stylesheet' type='text/css'>
+        <link rel='icon' type='image/png' sizes='64x64' href='https://thuwd.github.io/cdn/cdn/gba/img/404.png'>
+        <link href='https://fonts.googleapis.com/css2?family=Sen&display=swap' rel='stylesheet'>
+        <style>
+            }
+            .bruh {
+                font-family: 'Baloo Chettan 2', 'cursive';
+                text-align: center;
+                color: grey;
+            }
+            .fourohfour {
+                margin-top: 150px;
+                font-family: 'Source Code Pro', monospace;
+                font-size: 100px;
+                text-align: center;
+                color: #3d3d3d;
+                }
+        </style>
+    </head>"
