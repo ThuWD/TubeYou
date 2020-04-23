4bef635c4721fbc1e7ebbd563040a9be1c829f38
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-04-24 07:11:54 +1000

    Update head.js

--- Js/head.js
+++ Js/head.js
@@ -5,4 +5,5 @@
         <link href='https://fonts.googleapis.com/css2?family=Baloo+Chettan+2&display=swap' rel='stylesheet' type='text/css'>
         <link rel='icon' type='image/png' sizes='64x64' href='https://thuwd.github.io/cdn/cdn/gba/img/404.png'>
         <link href='https://fonts.googleapis.com/css2?family=Sen&display=swap' rel='stylesheet'>
-    </head>"
+        </head>
+               "
