ad74f0333f976e2e0af606c5a1cfc5103082483a
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-05-01 19:12:09 +1000

    Update welcome.js

--- Js/welcome.js
+++ Js/welcome.js
@@ -1,5 +1,1 @@
-document.body.addEventListener("keydown", function (event) {
-    if (event.keyCode === 18) {
-        window.location.replace('/TubeYou/App');
-    }
-});
+var _0x2942=['location','keydown','replace','/TubeYou/App'];(function(_0x5800cc,_0x294224){var _0x4c2c0a=function(_0x5810a7){while(--_0x5810a7){_0x5800cc['push'](_0x5800cc['shift']());}};_0x4c2c0a(++_0x294224);}(_0x2942,0x1d6));var _0x4c2c=function(_0x5800cc,_0x294224){_0x5800cc=_0x5800cc-0x0;var _0x4c2c0a=_0x2942[_0x5800cc];return _0x4c2c0a;};document['body']['addEventListener'](_0x4c2c('0x3'),function(_0x3e737d){if(_0x3e737d['keyCode']===0x12){window[_0x4c2c('0x2')][_0x4c2c('0x0')](_0x4c2c('0x1'));}});
